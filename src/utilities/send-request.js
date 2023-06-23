28717277d81195efcec8bdac37da82d4f8523367
usernamechasehils <chasehilsinger@gmail.com>
2023-06-23 16:18:19 -0400

    updated

--- src/utilities/send-request.js
+++ src/utilities/send-request.js
@@ -4,6 +4,7 @@ export default async function sendRequest(url, method = 'GET', payload = null) {
   // Fetch accepts an options object as the 2nd argument
   // used to include a data payload, set headers, specifiy the method, etc.
   const options = { method };
+  console.log('start sending request')
   if (payload) {
     options.headers = { 'Content-Type': 'application/json' };
     options.body = JSON.stringify(payload);
@@ -17,8 +18,11 @@ export default async function sendRequest(url, method = 'GET', payload = null) {
     // options.headers = options.headers || {};
     options.headers.Authorization = `Bearer ${token}`;
   }
+  console.log(payload)
   const res = await fetch(url, options);
+  console.log(res);
   // if res.ok is false then something went wrong
   if (res.ok) return res.json();
   throw new Error('Bad Request');
 }
+console.log('end sending request')
